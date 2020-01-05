918eac76e74d35877c62a8ab0f41c8cafc931367
Yahya <yahyatinani@gmail.com>
2020-01-05 22:37:51 +0100

    Refactor some tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,16 +2,13 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+    use WithoutMiddleware;
+
     public function testBasicTest()
     {
         $response = $this->get('/');
